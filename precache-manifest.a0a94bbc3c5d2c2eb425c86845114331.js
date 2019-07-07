e4481a15614c4861077e24ab2bfd1f30b79ab3db
Carter Edge <rezophonic@gmail.com>
2019-07-07 19:44:05 -0400

    Updates

---
+++ precache-manifest.a0a94bbc3c5d2c2eb425c86845114331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9621a76aebe58e1842ddf61fdca21841",
+    "url": "/wb/index.html"
+  },
+  {
+    "revision": "ccc972c9c2106d4978b6",
+    "url": "/wb/static/css/main.f5d6a214.chunk.css"
+  },
+  {
+    "revision": "9ba8cf053ace47f25ea6",
+    "url": "/wb/static/js/2.701cd602.chunk.js"
+  },
+  {
+    "revision": "ccc972c9c2106d4978b6",
+    "url": "/wb/static/js/main.55832ab0.chunk.js"
+  },
+  {
+    "revision": "04bee312f85045d7cbe8",
+    "url": "/wb/static/js/runtime~main.5aad80af.js"
+  }
+]);
